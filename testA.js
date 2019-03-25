d75abfc11fef68eba59eb30f126a099207003a6c
Rishi Karri <rishi.karri@turner.com>
2019-03-25 10:50:03 -0400

    express server

--- testA.js
+++ testA.js
@@ -1,1 +1,8 @@
-console.log('hi');
+var express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3456;
+app.use(express.static('./dist'));
+
+app.listen(PORT);
+console.log(`👂 Listening on http://localhost:${PORT}/ 👂`);
